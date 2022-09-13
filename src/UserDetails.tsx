cf803c4fe63044dc647ee9d3d4c2ee278e04b2f9
Christian Franco <c.franco@indexceed.com>
2022-09-13 13:11:08 -0500

    core: user info

--- src/UserDetails.tsx
+++ src/UserDetails.tsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom"
 import IonIcon from "@reacticons/ionicons"
 import './userStyles.css'
+import { useState } from "react"
 
 export const UserDetails = () => {
+
+    const [title, setTitle] = useState("Hi , My name is")
+    const [info, setInfo] = useState("Darren Kuhn")
+
   return (
     <div className="userMainContainer">
 
@@ -15,41 +20,41 @@ export const UserDetails = () => {
 
           <div className="infoContainer">
             <div className="infoTitle">
-                <span>Hi , My name is</span>
+                <span>{title}</span>
             </div>
             <div className="infoContent">
-                <span>Darren Kuhn</span>
+                <span>{info}</span>
             </div>
           </div>
 
           <div className="infoSelectorContainer">
             <div >
-              <button className="infoSelector">
+              <button className="infoSelector" onClick={() => {setTitle("Hi , My name is"), setInfo("Darren Kuhn")}}>
                 <IonIcon name="person" />
               </button>
             </div>
             <div >
-              <button className="infoSelector">
+              <button className="infoSelector" onClick={() => {setTitle("My email adress is"), setInfo("darren.kuhn@example.com")}}>
                 <IonIcon name="mail" />
               </button>
             </div>
             <div >
-              <button className="infoSelector">
+              <button className="infoSelector" onClick={() => {setTitle("My birthday is"), setInfo("8/3/1973")}}>
                 <IonIcon name="calendar" />
               </button>
             </div>
             <div >
-              <button className="infoSelector">
+              <button className="infoSelector" onClick={() => {setTitle("My adress is"), setInfo("7651 Lovers Ln")}}>
                 <IonIcon name="map" />
               </button>
             </div>
             <div >
-              <button className="infoSelector">
+              <button className="infoSelector" onClick={() => {setTitle("My phone number is"), setInfo("(846) 725-4715")}}>
                 <IonIcon name="call" />
               </button>
             </div>
             <div >
-              <button className="infoSelector">
+              <button className="infoSelector" onClick={() => {setTitle("My password is"), setInfo("marino")}}>
                 <IonIcon name="lock-closed" />
               </button>
             </div>
